24f6d255bc6b70c48c117c55bb0b9e92137390b8
KlyntonREP <clinkesh57@gmail.com>
2022-11-05 15:40:24 +0100

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors')
+
+
 const mathRouter = require("./routes/index");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", mathRouter);
